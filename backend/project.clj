78333f8b982a1a05958c7c202aa032d82210f40d
Lucas Canto <lucas.canto@siemens.com>
2021-02-16 22:03:53 -0300

    Updating versions

--- backend/project.clj
+++ backend/project.clj
@@ -3,17 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  ;; TODO: Replace versions to the more recent ones
-  ;; :dependencies [[org.clojure/clojure "1.10.1"]]
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  ;; Compojure - A basic routing library
-                 [compojure "1.6.1"]
+                 [compojure "1.6.2"]
                  ;; Our Http library for client/server
-                 [http-kit "2.3.0"]
+                 [http-kit "2.5.1"]
                  ;; Ring defaults - for query params etc
                  [ring/ring-defaults "0.3.2"]
                  ;; Clojure data.JSON library
-                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/data.json "1.0.0"]
                  ;; Static code analysis
                  [clj-kondo "RELEASE"]]
   :main ^:skip-aot backend.core
